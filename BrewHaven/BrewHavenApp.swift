656bf5834cbe981c29a5f9f645684bcf0f8d1345
ax958777 <ax958777@gmail.com>
2025-03-10 13:11:54 -0600

    Initial Commit

---
+++ BrewHaven/BrewHavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewHavenApp.swift
+//  BrewHaven
+//
+//  Created by AlexX on 2025-03-10.
+//
+
+import SwiftUI
+
+@main
+struct BrewHavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewHavenTests/BrewHavenTests.swift
@@ -0,0 +1,16 @@
+//
+//  BrewHavenTests.swift
+//  BrewHavenTests
+//
+//  Created by AlexX on 2025-03-10.
+//
+
+import Testing
+
+struct BrewHavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
